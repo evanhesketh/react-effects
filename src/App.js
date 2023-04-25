50062f0ffceca4706bdd3b69d96a0877b04f24c5
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-04-25 12:20:09 -0700

    added app docstring

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import Gameboard from './Gameboard';
+
+/**
+ * An awesome card drawing app in the browser!
+ *
+ * App -> Gameboard
+ */
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Gameboard />
+		</div>
+	);
 }
 
 export default App;
